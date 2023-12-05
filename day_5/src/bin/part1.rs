e033c5bc63a1b9153e0918b2c2d811832f729194
daic0r <daic0r.dev@gmail.com>
2023-12-05 09:48:47 +0100

    Part 1 works

--- day_5/src/bin/part1.rs
+++ day_5/src/bin/part1.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeMap;
 use regex::{Regex,Captures};
+use std::ops::Bound::*;
 
 #[derive(Debug)]
 struct Mapping {
@@ -39,18 +40,64 @@ impl Mapping {
 
         ret
     }
+
+    pub fn get(&self, src_idx: usize) -> usize {
+        let rng: Vec<(&usize, &(usize,usize))> = self.mapping.range((Unbounded, Included(src_idx))).collect();
+        println!("{:?}", rng);
+        if rng.is_empty() {
+            return src_idx;
+        }
+
+        let last = rng.last().unwrap();
+        let rng_len = last.1.1;
+        let src_rng_begin = last.0;
+        let dst_rng_begin = last.1.0;
+        if src_idx < src_rng_begin + rng_len {
+            return dst_rng_begin + (src_idx - src_rng_begin);
+        }
+
+        return src_idx;
+    }
 }
 
 fn main() {
-    let content = include_str!("../../input2.txt");
+    let content = include_str!("../../input.txt");
     
     let categories = content.split("\n\n");
     let mut mappings = vec![];
+    let mut seeds = vec![];
     for (idx,cat) in categories.enumerate() {
         if idx == 0 {
+            let rex = Regex::new(r"seeds:\s+(.*)").unwrap();
+            let caps = rex.captures(cat).unwrap();
+            let seed_nums = caps.get(1).unwrap().as_str().split(' ');
+            for seed in seed_nums {
+                seeds.push(seed.parse::<usize>().unwrap());
+            }
             continue;
         }
         mappings.push(Mapping::new(cat));
         println!("{:?}", mappings.last().unwrap());
+        
+        println!("{}", mappings.last().unwrap().get(50));
     }
+
+
+    let mut final_values = vec![];
+    for seed in seeds {
+        let mut src_cat = "seed";
+        let mut mapping = mappings.iter().find(|m| m.from == src_cat);
+        let mut src = seed;
+        while let Some(m) = mapping {
+            src = m.get(src);
+            src_cat = m.to.as_str();
+            mapping = mappings.iter().find(|m| m.from == src_cat);
+        }
+        final_values.push(src);
+    }
+
+    println!("{:?}", &final_values);
+
+    println!("Lowest location: {}", final_values.iter().min().unwrap());
+
 }
