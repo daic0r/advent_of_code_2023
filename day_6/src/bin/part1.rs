2f83e50b74f7c77aca4b308f3707838cbfa959a1
daic0r <daic0r.dev@gmail.com>
2023-12-06 08:17:18 +0100

    Part 1 works

--- day_6/src/bin/part1.rs
+++ day_6/src/bin/part1.rs
@@ -23,7 +23,7 @@ impl Game {
             // If we're currently in the range where we break the record and
             // suddenly drop below that number, the rest will also be below
             // the record => we can abort here
-            if dist_traveled <= self.duration && !self.times_button_to_win.is_empty() {
+            if dist_traveled <= self.record_dist && !self.times_button_to_win.is_empty() {
                 println!("Aborted at i={}", i);
                 break;
             }
@@ -35,7 +35,7 @@ impl Game {
 
 }
 fn main() {
-    let contents = include_str!("../../input2.txt");
+    let contents = include_str!("../../input.txt");
 
     let mut lines = contents.split('\n');
     let mut games = vec![];
