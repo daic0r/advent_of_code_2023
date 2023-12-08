de0a190be766dd3865463ab4a30feaab43773583
daic0r <daic0r.dev@gmail.com>
2023-12-08 18:47:55 +0100

    Part 2 works

--- day_8/src/bin/part2.rs
+++ day_8/src/bin/part2.rs
@@ -1,43 +1,39 @@
 use std::collections::HashMap;
 use regex::Regex;
+use std::{thread,time};
+use num::integer::lcm;
 
-fn count_steps(seq: &str, matrix: HashMap::<String, (String, String)>) -> usize {
+fn count_steps_to_end_state(start_node: &str, seq: &str, matrix: &HashMap::<String, (String, String)>) -> usize {
     let mut seq_idx = 0usize;
     let mut ret_step_cnt = 0usize;
 
-    let mut cur_nodes = matrix
+    let mut cur_node = matrix.get_key_value(start_node).unwrap();
+    while cur_node.0.chars().nth(2).unwrap() != 'Z' || seq_idx != 0 {
+        let nxt;
+        match seq.chars().nth(seq_idx).unwrap() {
+            'L' => nxt = cur_node.1.0.as_str(),
+            'R' => nxt = cur_node.1.1.as_str(),
+            _ => panic!("Should never happen")
+        };
+        cur_node = matrix.get_key_value(nxt).unwrap();
+        seq_idx = (seq_idx + 1) % seq.len();
+        ret_step_cnt += 1;
+    }
+    println!("{}: {}, {}", start_node, seq_idx, ret_step_cnt);
+    assert!(seq_idx == 0);
+    ret_step_cnt
+}
+fn count_steps(seq: &str, matrix: HashMap::<String, (String, String)>) -> usize {
+    let mut start_nodes = matrix
         .keys()
         .filter(|k| k.chars().nth(2).unwrap() == 'A')
         .map(|k| k.as_str())
         .collect::<Vec<&str>>();
-    for node in &cur_nodes {
-        println!("{}", node);
-    }
-    while !cur_nodes.iter().all(|k| k.chars().nth(2).unwrap() == 'Z') {
-        match seq.chars().nth(seq_idx).unwrap() {
-            'L' => {
-                cur_nodes = cur_nodes
-                    .iter()
-                    .map(|k| matrix
-                        .get(*k)
-                        .unwrap()
-                        .0.as_str())
-                    .collect();
-            }
-            'R' => {
-                cur_nodes = cur_nodes
-                    .iter()
-                    .map(|k| matrix
-                        .get(*k)
-                        .unwrap()
-                        .1.as_str())
-                    .collect();
-            }
-            _ => panic!("Should never happen")
-        };
-        seq_idx = (seq_idx + 1) % seq.len();
-        ret_step_cnt += 1;
-    }
+
+    let ret_step_cnt = start_nodes.iter()
+        .map(|str_node| count_steps_to_end_state(str_node, seq, &matrix))
+        .fold(1, |acc,x| lcm(acc, x));
+
     ret_step_cnt
 }
 
