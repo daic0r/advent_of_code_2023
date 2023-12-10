f66175fc6d96a3d13564535b5556e60434129318
daic0r <daic0r.dev@gmail.com>
2023-12-10 12:02:42 +0100

    Samples work

--- day_10/src/bin/part1.rs
+++ day_10/src/bin/part1.rs
@@ -18,15 +18,11 @@ const NEIGHBORS: [(Offset, Offset); 8] = [
     (Offset::Pos(1),   Offset::Pos(1))
 ];
 */
-const NEIGHBORS: [(isize, isize); 8] = [
-    (-1, -1),
+const NEIGHBORS: [(isize, isize); 4] = [
     (0,  -1),
-    (1,  -1),
     (-1,  0),
     (1,   0),
-    (-1,  1),
     (0,   1),
-    (1,   1)
 ];
 
 #[allow(dead_code)]
@@ -43,30 +39,106 @@ enum MapTile {
 }
 
 impl MapTile {
-    fn can_connect(&self, rhs: MapTile) -> bool {
+    fn can_connect(&self, rhs: MapTile, offs: (isize,isize)) -> bool {
+        use MapTile::*;
+
+        match self {
+            LeftToRight => match offs {
+                (-1, 0) => match rhs {
+                    Start | LeftToRight | TopToRight | BottomToRight => true,
+                    _ => false
+                },
+                (1, 0) => match rhs {
+                    Start | LeftToRight | TopToLeft | BottomToLeft => true,
+                    _ => false
+                },
+                _ => false
+            },
+            TopToBottom => match offs {
+                (0, -1) => match rhs {
+                    Start | TopToBottom | BottomToRight | BottomToLeft => true,
+                    _ => false
+                },
+                (0, 1) => match rhs {
+                    Start | TopToBottom | TopToRight | TopToLeft => true,
+                    _ => false
+                },
+                _ => false
+            },
+            TopToRight => match offs {
+                (1, 0) => match rhs {
+                    Start | LeftToRight | BottomToLeft | TopToLeft => true,
+                    _ => false
+                },
+                (0, -1) => match rhs {
+                    Start | TopToBottom | BottomToLeft | BottomToRight => true,
+                    _ => false
+                },
+                _ => false
+            },
+            TopToLeft => match offs {
+                (-1, 0) => match rhs {
+                    Start | LeftToRight | BottomToRight | TopToRight => true,
+                    _ => false
+                },
+                (0, -1) => match rhs {
+                    Start | TopToBottom | BottomToLeft | BottomToRight => true,
+                    _ => false
+                },
+                _ => false
+            },
+            BottomToRight => match offs {
+                (1, 0) => match rhs {
+                    Start | LeftToRight | TopToLeft | BottomToLeft => true,
+                    _ => false
+                },
+                (0, 1) => match rhs {
+                    Start | TopToBottom | TopToRight | TopToLeft => true,
+                    _ => false
+                },
+                _ => false
+            },
+            BottomToLeft => match offs {
+                (-1, 0) => match rhs {
+                    Start | LeftToRight | TopToRight | BottomToRight => true,
+                    _ => false
+                },
+                (0, 1) => match rhs {
+                    Start | TopToBottom | TopToLeft | TopToRight => true,
+                    _ => false
+                },
+                _ => false
+            },
+            Ground => false,
+            Start => match rhs {
+                Ground  => false,
+                _ => true
+            }
+        }
+        /*
         match self {
             Self::LeftToRight => match rhs {
-                Self::LeftToRight | Self::TopToRight | Self::TopToLeft | Self::BottomToLeft | Self::BottomToRight => true,
+                Self::Start | Self::LeftToRight | Self::TopToRight | Self::TopToLeft | Self::BottomToLeft | Self::BottomToRight => true,
                 _ => false
             },
             Self::TopToBottom => match rhs {
-                Self::TopToBottom | Self::TopToRight | Self::TopToLeft | Self::BottomToLeft | Self::BottomToRight => true,
+                Self::Start | Self::TopToBottom | Self::TopToRight | Self::TopToLeft | Self::BottomToLeft | Self::BottomToRight => true,
                 _ => false
             },
             Self::TopToRight => match rhs {
-                Self::LeftToRight | Self::TopToBottom | Self::BottomToRight | Self::BottomToLeft | Self::TopToLeft => true,
+                Self::Start | Self::LeftToRight | Self::TopToBottom | Self::BottomToRight | Self::BottomToLeft | Self::TopToLeft => true,
                 _ => false
             },
             Self::TopToLeft => match rhs {
-                Self::LeftToRight | Self::TopToBottom | Self::BottomToRight | Self::BottomToLeft | Self::TopToRight => true,
+                Self::Start | Self::LeftToRight | Self::TopToBottom | Self::BottomToRight | Self::BottomToLeft | Self::TopToRight => true,
                 _ => false
             },
             Self::BottomToRight => match rhs {
-                Self::LeftToRight | Self::TopToBottom | Self::TopToRight | Self::TopToLeft | Self::BottomToLeft => true,
+                Self::Start | Self::LeftToRight | Self::TopToBottom | Self::TopToRight | Self::TopToLeft | Self::BottomToLeft => true,
                 _ => false
             },
             Self::BottomToLeft => match rhs {
-                Self::LeftToRight | Self::TopToBottom | Self::TopToRight | Self::TopToLeft | Self::BottomToRight => true,
+                Self::Start | Self::LeftToRight | Self::TopToBottom | Self::TopToRight | Self::TopToLeft | Self::BottomToRight => true,
                 _ => false
             },
             Self::Ground => false,
@@ -75,10 +147,11 @@ impl MapTile {
                 _ => true
             }
         }
+        */
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Node {
     coord: (usize, usize),
     tile: MapTile,
@@ -113,8 +186,6 @@ impl Map {
             .collect(),
             nodes: LinkedList::new()
         };
-        ret.build_list();
-
         ret
     }
 
@@ -137,13 +208,67 @@ impl Map {
                 }
             });
         println!("Start at: {:?}", coord_start);
+
+        let start = self.data.get(coord_start.1).unwrap().get(coord_start.0).unwrap();
+        let adj_start = self.get_connected_neighbors(coord_start.0, coord_start.1);
+        // ((Coords), dist_to_start)
+        let mut path_1_last = Node {
+            dist_from_start: 0,
+            coord: coord_start,
+            tile: *start
+        };
+        let mut path_2_last = Node {
+            dist_from_start: 0,
+            coord: coord_start,
+            tile: *start
+        };
+        let mut path_1_cur = *adj_start.get(0).unwrap();
+        let mut path_2_cur = *adj_start.get(1).unwrap();
+        while path_1_cur.0 != path_2_cur.0 {
+            let tmp1 = self.get_connected_neighbors(path_1_cur.0.0, path_1_cur.0.1);
+            let tmp2 = self.get_connected_neighbors(path_2_cur.0.0, path_2_cur.0.1);
+
+            let neigh_1 = tmp1.iter()
+                .find(|(coord,tile)| *coord != path_1_last.coord)
+                .unwrap();
+            let neigh_2 = tmp2.iter()
+                .find(|(coord,tile)| *coord != path_2_last.coord)
+                .unwrap();
+
+            path_1_last = Node {
+                dist_from_start: path_1_last.dist_from_start + 1,
+                coord: path_1_cur.0,
+                tile: *path_1_cur.1
+            };
+            path_2_last = Node {
+                dist_from_start: path_2_last.dist_from_start + 1,
+                coord: path_2_cur.0,
+                tile: *path_2_cur.1
+            };
+
+            path_1_cur = *neigh_1;
+            path_2_cur = *neigh_2;
+        }
+        path_1_last = Node {
+            dist_from_start: path_1_last.dist_from_start + 1,
+            coord: path_1_cur.0,
+            tile: *path_1_cur.1
+        };
+        path_2_last = Node {
+            dist_from_start: path_2_last.dist_from_start + 1,
+            coord: path_2_cur.0,
+            tile: *path_2_cur.1
+        };
+
+        println!("Arrived at: {:?}", path_1_last);
+        println!("Arrived at: {:?}", path_2_last);
     }
 
-    fn get_tile(&self, x: usize, y: usize) -> MapTile {
-        *self.data.get(y).unwrap().get(x).unwrap()
+    fn get_tile(&self, x: usize, y: usize) -> &MapTile {
+        self.data.get(y).unwrap().get(x).unwrap()
     }
 
-    fn get_connected_neighbors(&self, x: usize, y: usize) -> Vec<((usize, usize), MapTile)> {
+    fn get_connected_neighbors(&self, x: usize, y: usize) -> Vec<((usize, usize), &MapTile)> {
         let this_tile = self.get_tile(x, y);
         let mut ret = vec![];
         for neighbor in &NEIGHBORS {
@@ -154,12 +279,13 @@ impl Map {
             if coord.0.unwrap() > self.data.first().unwrap().len()-1 || coord.1.unwrap() > self.data.len()-1 {
                 continue;
             }
-            let neighbor = self.get_tile(coord.0.unwrap(), coord.1.unwrap());
-            if this_tile.can_connect(neighbor) {
-                ret.push(((coord.0.unwrap(), coord.1.unwrap()), neighbor)); 
+            let neighbor_tile = self.get_tile(coord.0.unwrap(), coord.1.unwrap());
+            if this_tile.can_connect(*neighbor_tile, *neighbor) {
+                ret.push(((coord.0.unwrap(), coord.1.unwrap()), neighbor_tile)); 
             }
         }
-        assert!(ret.len() <= 2);
+        println!("Neighbors of {:?} are {:?}", (x, y), ret);
+        assert!(ret.len() == 2);
         ret
     }
 }
@@ -187,7 +313,7 @@ impl Display for Map {
 }
 
 fn main() {
-    let data = include_str!("../../input2.txt")
+    let data = include_str!("../../input3.txt")
         .split('\n')
         .filter_map(
             |l| match l.is_empty() { 
@@ -201,9 +327,11 @@ fn main() {
         .collect::<Vec<Vec<char>>>();
 
 
-        let map = Map::new(data.clone());
+        let mut map = Map::new(data.clone());
 
         println!("\n{}", map);
+
+        map.build_list();
 }
 
 #[cfg(test)]
