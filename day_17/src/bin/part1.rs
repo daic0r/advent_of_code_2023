19a3bf83b067e3bcd01f93281ae325e71d005b0c
daic0r <daic0r.dev@gmail.com>
2023-12-17 18:17:24 +0100

    Still not working

--- day_17/src/bin/part1.rs
+++ day_17/src/bin/part1.rs
@@ -5,6 +5,7 @@ struct Node {
     coord: Point,
     f_cost: usize,
     g_cost: usize,
+    symbol: Option<char>,
     prev: Option<Point>
 }
 
@@ -14,6 +15,7 @@ impl Node {
             coord: coord.clone(),
             f_cost: usize::MAX,
             g_cost: usize::MAX,
+            symbol: None,
             prev: None
         }
     }
@@ -38,7 +40,7 @@ struct Vector(isize, isize);
 
 const OFFSETS: [Vector; 4] = [ Vector(-1, 0), Vector(0, -1), Vector(1, 0), Vector(0, 1) ];
 
-fn find_path(map: &Map) -> Option<Vec<Point>> {
+fn find_path(map: &Map) -> Option<Vec<Node>> {
     let start = Point(0usize, 0usize);
     let dest = Point(map.first().unwrap().len()-1, map.len()-1);
     let h = |coord: &Point| (std::cmp::max(coord.0, dest.0) - std::cmp::min(coord.0, dest.0)) + (std::cmp::max(coord.1, dest.1) - std::cmp::min(coord.1, dest.1));
@@ -88,7 +90,7 @@ fn find_path(map: &Map) -> Option<Vec<Point>> {
         let mut last_dir_cnt = 0usize;
         let mut steps = 0usize;
         while let Some(from) = &cur.prev {
-            if steps == 3 {
+            if steps >= 3 {
                 break;
             }
             let vec_from_last = Vector(cur.coord.0 as isize - from.0 as isize, cur.coord.1 as isize - from.1 as isize);
@@ -96,12 +98,13 @@ fn find_path(map: &Map) -> Option<Vec<Point>> {
                 last_dir_cnt += 1;
             } else {
                 last_dir = vec_from_last;
-                last_dir_cnt = 0;
+                last_dir_cnt = 1;
             }
             cur = nodes.iter().find(|n| n.coord == *cur.prev.as_ref().unwrap()).unwrap();
             steps += 1;
         }
-        if last_dir_cnt == 3 {
+        if last_dir_cnt >= 3 {
+            println!("Found forbidden!!!!");
             Some(last_dir)
         } else {
             None
@@ -109,7 +112,7 @@ fn find_path(map: &Map) -> Option<Vec<Point>> {
     };
 
     let mut f_costs = BTreeMap::new();
-    f_costs.insert(h(&start), start);
+    f_costs.insert(h(&start), start.clone());
 
     while !f_costs.is_empty() {
         let f_costs_first = f_costs.pop_first().unwrap();
@@ -123,12 +126,14 @@ fn find_path(map: &Map) -> Option<Vec<Point>> {
 
         // Arrived at destination
         if current.coord == dest {
-            let mut ret = vec![ dest ];
-            let mut coord_cur = current.prev.clone();
-            while let Some(cur) = &coord_cur {
-                ret.push(cur.clone());
-                let cur_node = nodes.iter().find(|n| n.coord == *cur).unwrap();
-                coord_cur = cur_node.prev.clone();
+            let mut ret = vec![ current.clone() ];
+            let mut cur = current.prev;
+            while let Some(tmp) = cur {
+                let cur_node = nodes.iter().find(|n| n.coord == tmp).unwrap();
+                if tmp != start {
+                    ret.push(cur_node.clone());
+                }
+                cur = cur_node.prev.clone();
             }
             ret.reverse();
             return Some(ret);
@@ -140,10 +145,11 @@ fn find_path(map: &Map) -> Option<Vec<Point>> {
             if let Some(prev) = &current.prev {
                 assert!(*prev != neighbor);
             }
+            let dir_to_here = Vector(neighbor.0 as isize - current.coord.0 as isize, neighbor.1 as isize - current.coord.1 as isize);
             if let Some(forbidden) = &forbidden {
-                if Vector(neighbor.0 as isize - current.coord.0 as isize, neighbor.1 as isize - current.coord.1 as isize)
-                    == *forbidden
+                if dir_to_here == *forbidden
                 {
+                    println!("Skipped because forbidden!");
                     continue;
                 }
             }
@@ -159,6 +165,13 @@ fn find_path(map: &Map) -> Option<Vec<Point>> {
                 neighbor_node.prev = Some(current.coord.clone());
                 neighbor_node.g_cost = g_cost;
                 neighbor_node.f_cost = g_cost + h(&neighbor);
+                neighbor_node.symbol = match dir_to_here {
+                    Vector(-1, 0) => Some('<'),
+                    Vector(0, -1) => Some('^'),
+                    Vector(1, 0) => Some('>'),
+                    Vector(0, 1) => Some('v'),
+                    _ => panic!("Shouldn't happen")
+                };
                 if !f_costs.iter().any(|kvp| *kvp.1 == neighbor) {
                     f_costs.insert(neighbor_node.f_cost, neighbor);
                 }
@@ -184,21 +197,15 @@ fn main() {
         }
         for (row_idx,row) in map.iter().enumerate() {
             for (col_idx,col) in row.iter().enumerate() {
-                if path.iter().any(|p| *p == Point(col_idx,row_idx)) {
-                    print!("X");
+                if let Some(node) = path.iter().find(|p| p.coord == Point(col_idx,row_idx)) {
+                    print!("{}", node.symbol.unwrap());
                 } else {
                     print!("{}", map[row_idx][col_idx]);
                 }
             }
             println!();
         }
-        let heat_loss = path.iter().map(|p| {
-            if p.1 != 0 || p.0 != 0 {
-                map[p.1][p.0].to_digit(10).unwrap() as usize
-            } else {
-                0
-            }
-        }).sum::<usize>();
+        let heat_loss = path.iter().map(|p| map[p.coord.1][p.coord.0].to_digit(10).unwrap() as usize).sum::<usize>();
         println!("Length of path: {}", path.len());
         println!("Heat loss = {}", heat_loss);
     }
