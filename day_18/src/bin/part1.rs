cb3ef5c0229c310b886df57f960d09d3f990709c
daic0r <daic0r.dev@gmail.com>
2023-12-18 19:56:53 +0100

    Draw the map

--- day_18/src/bin/part1.rs
+++ day_18/src/bin/part1.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Direction {
     Up(usize),
     Down(usize),
@@ -8,7 +8,7 @@ enum Direction {
     Left(usize),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Instruction {
     dig: Direction,
     color: String
@@ -34,13 +34,109 @@ impl Instruction {
         }
     }
 }
+
+fn get_dims(instr: &Vec<Instruction>) -> (usize, usize) {
+    use Direction::*;
+    let mut min_x = usize::MAX;
+    let mut max_x = usize::MIN;
+    let mut min_y = usize::MAX;
+    let mut max_y = usize::MIN;
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+    for i in instr {
+        match i.dig {
+            Left(n) => x = x.checked_sub(n).unwrap(),
+            Right(n) => x = x.checked_add(n).unwrap(),
+            Up(n) => y = y.checked_sub(n).unwrap(),
+            Down(n) => y = y.checked_add(n).unwrap()
+        };
+        min_x = std::cmp::min(min_x, x);
+        max_x = std::cmp::max(max_x, x);
+        min_y = std::cmp::min(min_y, y);
+        max_y = std::cmp::max(max_y, y);
+    }
+    max_x += 1;
+    max_y += 1;
+
+    (max_x, max_y)
+}
+
+fn draw_map(instr: &Vec<Instruction>, map: &mut Vec<Vec<char>>) {
+    use Direction::*;
+
+    let mut x = 0usize;
+    let mut y = 0usize;
+    for i in instr {
+        match i.dig {
+            Left(n) | Right(n) => {
+                println!("{:?}", i.dig);
+                let step;
+                let to_x = if let Left(_) = i.dig {
+                    let tmp = x.checked_sub(n).unwrap();
+                    step = -1isize;
+                    tmp
+                } else {
+                    let tmp = x.checked_add(n).unwrap();
+                    step = 1isize;
+                    tmp
+                };
+                while x != to_x {
+                    map[y][x] = '#'; 
+                    x = x.checked_add_signed(step).unwrap();
+                }
+            },
+            Up(n) | Down(n) => {
+                println!("{:?}", i.dig);
+                let step;
+                let to_y = if let Up(_) = i.dig {
+                    let tmp = y.checked_sub(n).unwrap();
+                    step = -1isize;
+                    tmp
+                } else {
+                    let tmp = y.checked_add(n).unwrap();
+                    step = 1isize;
+                    tmp
+                };
+                while y != to_y {
+                    map[y][x] = '#'; 
+                    y = y.checked_add_signed(step).unwrap();
+                }
+            },
+        };
+
+    }
+}
+
 fn main() {
+    use Direction::*;
+
     let instr = include_str!("../../input2.txt")
         .lines()
         .map(Instruction::new)
         .collect::<Vec<Instruction>>();
     
-    for i in instr {
+    for i in &instr {
         println!("{:?}", i);
+    }
+
+    let (max_x, max_y) = get_dims(&instr);
+    println!("Dimensions: {}, {}", max_x, max_y);
+
+    let mut map = std::iter::repeat(
+            Vec::from_iter(std::iter::repeat('.')
+                .take(max_x)
+                .collect::<Vec<char>>()
+            ))
+            .take(max_y)
+            .collect::<Vec<Vec<char>>>();
+
+
+    draw_map(&instr, &mut map);
+
+    for line in &map {
+        for ch in line {
+            print!("{}", ch);
+        }
+        println!();
     }
 }
