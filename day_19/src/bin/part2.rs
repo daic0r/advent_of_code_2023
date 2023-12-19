ec4a9dabd15ba677864f061b07360136ad957498
daic0r <daic0r.dev@gmail.com>
2023-12-19 21:40:41 +0100

    Part 2 works

--- day_19/src/bin/part2.rs
+++ day_19/src/bin/part2.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::hash::Hash;
-use std::ops::{RangeBounds, RangeInclusive};
+use std::ops:: RangeInclusive;
 use regex::Regex;
 use std::fmt::Display;
 
@@ -13,22 +13,22 @@ enum Rule {
 }
 
 impl Rule {
-    fn adapt_range(&self, coll: &mut PartCollection) -> Option<&str> {
+    fn adapt_range(&self, coll: &mut PartCollection, coll_inv: &mut PartCollection) -> Option<&str> {
         use Rule::*;
         match self {
             Accept => return Some("A"),
             Reject=> return Some("R"),
             _ => {}
         };
-        let range = match self {
+        let (range, inv_range) = match self {
             LessThan((cat,val,to)) | GreaterThan((cat,val,to)) => {
-                coll.ranges.get_mut(cat).unwrap()
+                (coll.ranges.get_mut(cat).unwrap(), coll_inv.ranges.get_mut(cat).unwrap())
             },
             _ => panic!("Impossible")
         };
-        let (my_range, to) = match self {
-            LessThan((cat,val,to)) => (1..=val-1, to),
-            GreaterThan((cat,val,to)) => (val+1..=4000, to),
+        let (my_range, my_inv_range, to) = match self {
+            LessThan((cat,val,to)) => (1..=val-1, *val..=4000, to),
+            GreaterThan((cat,val,to)) => (val+1..=4000, 1..=*val, to),
             _ => panic!("Impossible")
         };
         if range.start() < my_range.start() && range.end() < my_range.start() {
@@ -37,6 +37,8 @@ impl Rule {
         if range.start() > my_range.end() && range.end() > my_range.end() {
             return None;
         }
+        *inv_range = RangeInclusive::new(*std::cmp::max(range.start(), my_inv_range.start()),
+            *std::cmp::min(range.end(), my_inv_range.end()));
         *range = RangeInclusive::new(*std::cmp::max(range.start(), my_range.start()),
             *std::cmp::min(range.end(), my_range.end()));
 
@@ -93,49 +95,10 @@ impl From<&str> for Category {
 }
 
 #[derive(Debug)]
-struct Part {
-    categories: HashMap<Category, u32> 
-}
-
-impl From<&str> for Part {
-    fn from(value: &str) -> Self {
-        let mut categories = HashMap::new();
-        value[1..value.len()-1].split(",")
-            .map(|s| {
-                let val = s[2..].parse::<u32>().unwrap();
-                (Category::from(&s[0..1]), val)
-            })
-            .for_each(|(cat,val)| { categories.insert(cat, val); });
-        Self {
-            categories
-        }
-    }
-}
-
-#[derive(Debug)]
 struct Workflow {
     name: String,
     rules: Vec<Rule>,
     final_dest: String
-}
-
-impl Workflow {
-    // fn process(&self, p: &Part) -> &str {
-    //     for rule in &self.rules {
-    //         if let Some(to) = rule.process(p) {
-    //             return to;
-    //         }
-    //     }
-    //     self.final_dest.as_str()
-    // }
-
-    fn rules(&self) -> &Vec<Rule> {
-        &self.rules
-    }
-
-    fn final_dest(&self) -> &str {
-        &self.final_dest
-    }
 }
 
 impl From<&str> for Workflow {
@@ -196,18 +159,19 @@ impl PartCollection {
             ranges
         }
     }
+
+    fn get_combination_count(&self) -> u128 {
+        self.ranges.values().fold(1, |acc,x| acc * (x.end() - x.start() + 1) as u128)
+    }
 }
 
-fn get_combination_count(coll: &PartCollection) -> u128 {
-    coll.ranges.values().fold(1, |acc,x| acc * (x.end() - x.start() + 1) as u128)
-}
-fn fit_through(workflows: &HashMap<String, Workflow>) -> Vec<PartCollection> {
+fn fit_ranges(workflows: &HashMap<String, Workflow>) -> Vec<PartCollection> {
     let mut ret = vec![];
 
-    let mut the_stack = vec![ ("in", PartCollection::new()) ];
+    let mut the_stack = vec![ ("in", "", PartCollection::new()) ];
     while !the_stack.is_empty() {
-        let (wf_name, coll)  = the_stack.pop().unwrap();
-        println!("-> {}, {}", wf_name, coll);
+        let (wf_name, from, coll) = the_stack.pop().unwrap();
+        println!("{}->{}, {}", from, wf_name, coll);
         if wf_name == "R" {
             continue;
         } else 
@@ -216,30 +180,27 @@ fn fit_through(workflows: &HashMap<String, Workflow>) -> Vec<PartCollection> {
             continue;
         }
 
-
         let wf = workflows.get(wf_name).unwrap();
+        // inv_coll is the "inverted range" of what is passed into the next
+        // processing state, i.e. if s < 400 is passed onto the next stage,
+        // we only have s >= 400 for the next the rule. inv_coll is that s >= 400
+        let mut inv_coll = coll.clone();
         for rule in &wf.rules {
-            let mut this_coll = coll.clone();
-            let nxt = rule.adapt_range(&mut this_coll);
+            let mut this_coll = inv_coll.clone();
+            let nxt = rule.adapt_range(&mut this_coll, &mut inv_coll);
             if let Some(nxt) = nxt {
-                if nxt == "A" {
-                    ret.push(this_coll);
-                } else {
-                    the_stack.push((nxt, this_coll));
-                }
+                the_stack.push((nxt, wf_name, this_coll));
             }
         }
-        the_stack.push((&wf.final_dest, coll.clone()));
+        the_stack.push((&wf.final_dest, wf_name, inv_coll.clone()));
     }
     ret
 }
 
 fn main() {
-    let contents = include_str!("../../input2.txt");
+    let contents = include_str!("../../input.txt");
 
     let idx_separator = contents.lines().position(|l| l.is_empty()).unwrap();
-
-    println!("Separator at {}", idx_separator);
 
     let mut workflows: HashMap<String, Workflow> = HashMap::new();
     contents
@@ -254,8 +215,19 @@ fn main() {
 
     println!();
 
-    let combos = fit_through(&workflows);
+    let combos = fit_ranges(&workflows);
     println!("Got {} ranges", combos.len()); 
-    let sum = combos.iter().fold(1, |acc,x| acc + get_combination_count(x));
+    let sum = combos.iter().fold(0, |acc,x| acc + x.get_combination_count());
     println!("Number of combinations = {}", sum);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calc_combinations() {
+        let coll = PartCollection::new();
+        assert_eq!(coll.get_combination_count(), 256000000000000);
+    }
 }
