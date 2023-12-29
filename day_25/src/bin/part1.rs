db1f40a7c5d52435217babf9c636d968e36f5d62
daic0r <daic0r.dev@gmail.com>
2023-12-29 23:03:24 +0100

    Part 1 works

--- day_25/src/bin/part1.rs
+++ day_25/src/bin/part1.rs
@@ -22,20 +22,10 @@ impl<'a> VertexGroup<'a> {
             .fold(HashMap::new(), |mut acc,x| {
                 //acc.extend(x.1.iter().filter(|&&s| !self.vertices.contains(s)));
                 x.1.iter()
-                    .filter(|(s,w)| !self.vertices.contains(s.as_str()))
+                    .filter(|(s,_)| !self.vertices.contains(s.as_str()))
                     .for_each(|(s,w)| {
                         acc.entry(&s).and_modify(|m| *m+=*w).or_insert(*w);
                     });
-                acc
-            })
-    }
-
-    fn external_nodes(&self) -> HashSet<&'a str> {
-        self.vertex_set
-            .iter()
-            .filter(|(node, _)| !self.vertices.contains(node.as_str()))
-            .fold(HashSet::new(), |mut acc,x| {
-                acc.insert(x.0);
                 acc
             })
     }
@@ -67,7 +57,7 @@ fn merge_nodes(node1: &str, node2: &str, graph: &mut Graph) {
     }
 }
 
-fn stoer_wagner(graph: &Graph) -> (Vec<String>, Vec<String>) {
+fn stoer_wagner(graph: &Graph) -> Vec<String> {
     let mut graph_copy = graph.clone();
     let mut cuts = vec![];
     let mut idx = 0;
@@ -90,29 +80,32 @@ fn stoer_wagner(graph: &Graph) -> (Vec<String>, Vec<String>) {
                 ret
             };
             let max_edge = external_edges.iter().max_by_key(|(_,w)| *w);
-            println!("Adding {}, weight {}", *max_edge.unwrap().0, *max_edge.unwrap().1);
+            //println!("Adding {}, weight {}", *max_edge.unwrap().0, *max_edge.unwrap().1);
             second_to_last_insert = last_insert;
             last_insert = Some(*max_edge.unwrap().0);
             last_weight = Some(*max_edge.unwrap().1);
             vg.vertices.insert(*max_edge.unwrap().0);
+            //println!("Group after {}: {:?}", idx, vg.vertices);
         }
-        println!("{:?} and {:?}", last_insert.unwrap(),  second_to_last_insert.unwrap());
-        cuts.push((graph_copy.clone(), last_weight.unwrap(), last_insert.unwrap().to_string(), second_to_last_insert.unwrap().to_string(), idx));
+        //println!("{:?} and {:?}", last_insert.unwrap(),  second_to_last_insert.unwrap());
+        cuts.push((vg.vertices.iter().map(|s| s.to_string()).collect::<Vec<_>>(), last_weight.unwrap(), last_insert.unwrap().to_string(), second_to_last_insert.unwrap().to_string(), idx));
         merge_nodes(&last_insert.unwrap(), &second_to_last_insert.unwrap(), &mut graph_copy);
-        println!("{:?}", graph_copy);
-        println!("---------------------------------------------");
+        println!("Nodes in graph after {}: {}", idx, graph_copy.len());
+        //println!("{:?}", graph_copy);
+        //println!("---------------------------------------------");
         idx += 1;
+        if last_weight.unwrap() == 3 {
+            break;
+        }
     }
     let min_cut = cuts.iter().min_by_key(|kvp| kvp.1).unwrap();
-    let mut vg = VertexGroup::new(&graph); 
-    for sp in min_cut.2.split(",") {
-        vg.vertices.insert(sp);
-    }
-    (vg.edges().keys().map(|s| s.to_string()).collect(), vg.external_nodes().iter().map(|s| s.to_string()).collect())
+    println!("Minimum is index {}", min_cut.4);
+    let group_one = min_cut.0.iter().max_by_key(|node_cluster| node_cluster.len()).unwrap();
+    group_one.split(",").map(|s| s.to_string()).collect::<Vec<_>>()
 }
 
 fn main() {
-    let contents = std::fs::read_to_string("input2.txt").unwrap();
+    let contents = std::fs::read_to_string("input.txt").unwrap();
 
     let mut adj_list: Graph = Graph::new();
 
@@ -135,15 +128,11 @@ fn main() {
             .or_insert(con_set.into_iter().map(|s| (s,  1)).collect());
     }
 
-    let mut vg = VertexGroup {
-        vertex_set: &adj_list,
-        vertices: HashSet::new()
-    };
-
-    vg.vertices.insert("rhn");
-    vg.vertices.insert("bvb");
+    println!("{}", adj_list.len());
 
     println!("{:?}", adj_list);
-    let two_sets = stoer_wagner(&adj_list);
-    println!("Separated sets: {:?}", two_sets);
+    let one_group = stoer_wagner(&adj_list);
+    println!("Group: {:?}", one_group);
+    let product = one_group.len() * (adj_list.len() - one_group.len());
+    println!("Product: {}", product);
 }
